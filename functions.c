18b06a78d3a556b9eb14ebd6bbaf95906f356a2a
SenseiDante <dnlmusumba@gmail.com>
2023-10-17 18:30:26 -0700

    functions

--- functions.c
+++ functions.c
@@ -72,7 +72,31 @@ int print_string(va_list types, char buffer[],
 	}
 	return (write(1, str, length));
 }
-/*******************PRINT PERCENTAGE SIGN**********************/
+/*******************PRINT PERCENT SIGN**********************/
+/**
+ * print_percent - print percent sign
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags: calculates active flags
+ * @width: width
+ * @precision: precision specs
+ * @size: size of specifier
+ * Return: Number of chars printed
+ */
+
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+/*******************PRINT INT**********************/
 /**
  * print_int - print int
  * @types: list of arguments
